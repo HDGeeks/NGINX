37baf2edcc6b472f43599638b6305249f7aa211c
Daniel Tesfai <dannyhd88@gmail.com>
2024-10-02 22:20:54 +0200

    editing resource

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -17,21 +17,39 @@ provider "azurerm" {
 module "network" {
   source = "./network"
 }
+
 output "network_rg_name" {
   value = module.network.resource_group_name
 }
 
+output "network_rg_location" {
+  value = module.network.resource_group_location
+}
+output "network_rg_id" {
+  value = module.network.resource_group_id  
+}
+
+
+
 module "vm" {
   source = "./vm"
-  depends_on = [module.network]
+  depends_on = [module.network,module.storage,module.ssh]
 }
 
 module "storage" {
   source = "./storage"
-  depends_on = [module.vm]
+  depends_on = [module.vm,module.network]
 }
 
 module "ssh" {
   source = "./ssh"
   depends_on = [module.network]
+  resource_group_location = module.network.resource_group_location
+  resource_group_name = module.network.resource_group_name
+  resource_group_id = module.network.resource_group_id
+
+
+
+
+
 }
--- Terraform/ssh/ssh.tf
+++ Terraform/ssh/ssh.tf
@@ -6,7 +6,7 @@ resource "random_pet" "ssh_key_name" {
 resource "azapi_resource" "ssh_public_key" {
   type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
   name      = random_pet.ssh_key_name.id
-  location  = azurerm_resource_group.rg.location
+  location  = var.resource_group_location
   parent_id = azurerm_resource_group.rg.id
 }
 
